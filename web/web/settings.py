f08e405c07534a680c3b618c240bae6d133caccb
Paul Hallett <paulandrewhallett@gmail.com>
2012-08-07 15:48:26 +0100

    Fixed styling

--- web/web/settings.py
+++ web/web/settings.py
@@ -61,20 +61,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/web/css/',
-    '/web/imgs/',
-)
+STATICFILES_DIRS = [os.path.join(SITE_ROOT, dd) for dd in \
+    ('css', 'img',)\
+]
 
 # List of finder classes that know how to find static files in
 # various locations.
